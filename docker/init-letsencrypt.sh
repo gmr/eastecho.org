fdc69b8e55fd7cc568b48feb02e5712f6487543c
Gavin M. Roy <gavinr@aweber.com>
2019-10-01 00:44:06 -0400

    Fix the init-certbot stuffs

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -7,9 +7,9 @@ fi
 
 domains=(eastecho.org www.eastecho.org)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="/opt/eastecho/certbot"
 email="gmr@poisonpenllc.com"
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
